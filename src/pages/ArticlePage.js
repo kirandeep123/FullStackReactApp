c4888b8a89c3ebe0133e688b520686d17f01d759
kirandeep123 <me@kiran.cc>
2019-07-27 16:50:04 +0530

    route added

---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ArticlePage = () => (
+  <React.Fragment>
+   
+    <h1>Articles Page</h1>
+    
+
+    </React.Fragment>
+    
+    );
+
---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const ArticleList = () => (
+  <React.Fragment>
+   
+    <h1>Articles List</h1>
+    
+
+    </React.Fragment>
+    
+    );
+
