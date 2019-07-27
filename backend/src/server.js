3beb6a9b30f123e21cf7aa3bfb755607397d1dac
kirandeep123 <me@kiran.cc>
2019-07-27 22:03:40 +0530

    changes made will see tomorrow

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,36 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
+import {MongoClient} from 'mongodb';
 const app =express();
 
-const articleInfo ={
-    'learn-react':{
-        upvotes:0,
-        comments:[]
-    },
-    'learn-node':{
-        upvotes:0,
-        comments:[]
 
-    },
-    'my-thoughts':{
-        upvotes:0,
-        comments:[]
-
-    }
-}
 app.use(bodyParser.json());
 
-app.post("/api/articles/:name/upvote",(req,res)=>{
+app.get("/api/articles/:name", async(req,res)=>{
+    try{
+    const articleName = req.params.name;
+  const client=await  MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser:true})
+    const db =client.db('fullstack')
+    const articleInfo = await (db.collection('articles').findOne({name:articleName}))
+    res.status(200).json(articleInfo)
+    client.close()
+    }catch( e ){
+        res.status(500).json({message:'error'});
+       
+   
+        
+    }
+})
+
+app.post("/api/articles/:name/upvote",async(req,res)=>{
         const articleName = req.params.name   
-        articleInfo[articleName].upvotes++;
-    res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes!`)
+        const client=await  MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser:true})
+        const db =client.db('fullstack')
+        const articleInfo = await db.collection('articles').findOne({name:articleName});
+         await db.collection('articles').updateOne({name:articleName},
+                        '$set': {
+                            upvotes:articleInfo.upvotes+1
+                            });
 })
 
 app.post("/api/articles/:name/comment",(req,res)=>{
