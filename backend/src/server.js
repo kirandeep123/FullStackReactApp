d27cab4ca38bf9add9f1bee257c814d6575661fa
kirandeep123 <me@kiran.cc>
2019-07-27 21:06:04 +0530

    backend changes

---
+++ backend/src/server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app =express();
+
+const articleInfo ={
+    'learn-react':{
+        upvotes:0,
+        comments:[]
+    },
+    'learn-node':{
+        upvotes:0,
+        comments:[]
+
+    },
+    'my-thoughts':{
+        upvotes:0,
+        comments:[]
+
+    }
+}
+app.use(bodyParser.json());
+
+app.post("/api/articles/:name/upvote",(req,res)=>{
+        const articleName = req.params.name   
+        articleInfo[articleName].upvotes++;
+    res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes} upvotes!`)
+})
+
+app.post("/api/articles/:name/comment",(req,res)=>{
+    const articleName =req.params.name;
+    const {username,text} =req.body;
+    articleInfo[articleName].comments.push({username,text})
+    res.status(200).send(articleInfo[articleName])
+})
+
+app.listen(8000,()=>{
+    console.log("app is listening")
+})
